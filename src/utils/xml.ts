e5193589947ee65997a181007c596cc1e2f1bfb9
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-11 21:01:21 +0000

    chore: upgrade dependencies

--- src/utils/xml.ts
+++ src/utils/xml.ts
@@ -1,24 +1,11 @@
-import {
-  removeMultilineHeader
-, removeMultilineFooter
-, removeFalsyValues
-, removeExtraIndents
-, toString
-, removeBlankLines
-} from 'extra-tags'
+import { removeBlankLines, removeFalsyValues, dedent } from 'extra-tags'
 import { Falsy } from 'justypes'
 
 export function xml(strings: TemplateStringsArray, ...values: Array<string | Falsy>) {
   return (
-    removeExtraIndents(
-      removeBlankLines(
-        removeMultilineFooter(
-          removeMultilineHeader(
-            toString(
-              ...removeFalsyValues(strings, ...values)
-            )
-          )
-        )
+    removeBlankLines(
+      dedent(
+        ...removeFalsyValues(strings, ...values)
       )
     )
   )
